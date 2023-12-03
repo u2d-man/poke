9afe7e72da136ddd68601ea8fcb00dcce184af3f
UserKazun <developer.kazu0941@gmail.com>
2023-12-03 09:59:31 +0900

    add api class

---
+++ frontend/src/libs/Apis.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+const baseUrl = 'http://localhost:8080'
+
+class Apis {
+    async getPokemonBasicInfo(pokedexID: number, axiosconfig?: AxiosRequestConfig) {
+        const { data } = await axios.get<ApiResponse>(`${baseUrl}/api/v1/pokemon/${pokedexID}`, axiosconfig);
+
+        return data
+    }
+}
+
+const apis = new Apis();
+export default apis;
+
+export interface ApiResponse {
+    message: string
+    data: string[]
+}
