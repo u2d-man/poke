384d2db481d8dd23b13ee8fe6ae0dfab5412eb6d
UserKazun <developer.kazu0941@gmail.com>
2023-12-10 14:13:44 +0900

    update response

--- frontend/src/libs/Apis.ts
+++ frontend/src/libs/Apis.ts
@@ -1,10 +1,14 @@
 import axios, { AxiosRequestConfig } from "axios";
+import PokemonBaseInfo from "../components/PokemonBaseInfo";
 
 const baseUrl = 'http://localhost:8080'
 
 class Apis {
     async getPokemonBasicInfo(pokedexID: number, axiosconfig?: AxiosRequestConfig) {
-        const { data } = await axios.get<ApiResponse>(`${baseUrl}/api/v1/pokemon/${pokedexID}`, axiosconfig);
+        const { data } = await axios.get<PokeBaseInfoResponse>(
+            `${baseUrl}/api/v1/pokemon/${pokedexID}`,
+            axiosconfig
+        );
 
         return data;
     }
@@ -23,3 +27,17 @@ export interface ApiResponse {
     message: string
     data: any[]
 }
+
+export interface PokeBaseInfoResponse {
+    message: string
+    data: PokeBaseInfo
+}
+
+export interface PokeBaseInfo {
+    front_img: string
+    name: string
+    types: string[]
+    height: number
+    weight: number
+}
+
