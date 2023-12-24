64ae52ce7006eec440638058022c8c395228b221
UserKazun <developer.kazu0941@gmail.com>
2023-12-24 14:40:30 +0900

    fix layout

--- frontend/src/components/PokemonBaseInfo.tsx
+++ frontend/src/components/PokemonBaseInfo.tsx
@@ -1,6 +1,9 @@
-import {useEffect, useState} from "react";
-import apis, {ApiResponse, PokeBaseInfoResponse} from "../libs/Apis";
+import React, {useEffect, useState} from "react";
+import apis, {PokeBaseInfoResponse} from "../libs/Apis";
 import TypeCard from "./TypeCard";
+import {Chart as ChartJS, LineElement, PointElement, RadarController, RadialLinearScale} from "chart.js";
+
+ChartJS.register(RadarController, LineElement, PointElement, RadialLinearScale);
 
 const PokemonBaseInfo = () => {
     const [baseInfo, setBaseInfo] = useState<PokeBaseInfoResponse>();
@@ -11,20 +14,18 @@ const PokemonBaseInfo = () => {
         fetchBaseInfo();
     }, []);
 
-    console.log(baseInfo)
-
     return (
-        <div className="container md:flex">
-            <div className="box-content h-60 w-60 p-4 m-10 border-4 rounded-md border-indigo-500">
-                <img src={baseInfo?.data.front_img} className="w-60"  alt="icon" />
+        <div className="flex border-4 m-5 w-3/6 absolute">
+            <div className="box-content w-60 p-4 m-10 border-4 rounded-md border-indigo-500">
+                <img src={baseInfo?.data.front_img} className="w-60" alt="icon"/>
             </div>
-            <div className="box-content p-4 m-5 mr-auto text-left">
-                <p className="text-lg font-bold">{ baseInfo?.data.name }</p>
+            <div className="box-content p-4 m-5 text-left">
+                <p className="text-lg font-bold">{baseInfo?.data.name}</p>
                 <div className="pt-6">
-                    <p>全国No. { baseInfo?.data.pokedex_id }</p>
-                    <p>高さ { baseInfo?.data.height }m</p>
-                    <p className="pb-6">重さ { baseInfo?.data.weight }kg</p>
-                    <TypeCard types={ baseInfo?.data.types } />
+                    <p>全国No. {baseInfo?.data.pokedex_id}</p>
+                    <p>高さ {baseInfo?.data.height}m</p>
+                    <p className="pb-6">重さ {baseInfo?.data.weight}kg</p>
+                    <TypeCard types={baseInfo?.data.types}/>
                 </div>
             </div>
         </div>
