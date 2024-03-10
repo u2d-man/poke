a310391ef332680b55c763322132f15e6c70145c
UserKazun <developer.kazu0941@gmail.com>
2024-03-10 19:32:07 +0900

    use QueryClient

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,30 @@ import React from 'react';
 import './App.css';
 import Home from "./pages/Home";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: false,
+        },
+    },
+});
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-            <Routes>
-                <Route path='/home' element={
-                    <Home pokedexID={ 1000 } />
-                }>
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/home' element={
+                        <Home pokedexID={ 1000 } />
+                    }>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
     </div>
   );
 }
