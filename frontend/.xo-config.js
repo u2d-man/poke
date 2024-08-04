be802c114b7b9dc2abd98086e8cdfa468ec4073a
UserKazun <developer.kazu0941@gmail.com>
2024-08-04 12:27:07 +0900

    add xo, and fix format error

---
+++ frontend/.xo-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: ["xo-react"],
+  rules: {
+    // コンポーネントのファイル名はPascalCase、hooksのファイル名はcamelCaseを使いたいので有効にしておく
+    'unicorn/filename-case': [
+      'error',
+      {
+        cases: {
+          pascalCase: true,
+          camelCase: true,
+        },
+      },
+    ],
+    'unicorn/prevent-abbreviations': 'off',
+    // import文で拡張子チェックはいらない
+    'import/extensions': 'off',
+    // React 17以降はJSXの対応済みのための'React'のimportはいらない
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    // 以下 好み
+    // 関数の引数は2つまで。3以上だとオブジェクトにしてまとめる
+    'max-params': ['error', 2],
+  },
+  // 以下 好み
+  // 末尾にセミコロンはつけない
+  semicolon: true,
+  // prettierを有効にする
+  prettier: true,
+  space: 2
+}
